cded0cd017e43bbd279847b3328409a024cd3b90
sgreene30 <sgreene30@Ubuntu-Fresh.myguest.virtualbox.org>
2023-02-25 13:31:52 -0500

    fixed file paths in finder-test.sh

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -8,7 +8,8 @@ set -u
 NUMFILES=10
 WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
-username=$(cat /etc/finder-app/conf/username.txt)
+CONFIG_PATH = /etc/finder-app/conf
+username=$(cat ${CONFIG_PATH}/username.txt)
 
 if [ $# -lt 3 ]
 then
@@ -32,7 +33,7 @@ echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
 rm -rf "${WRITEDIR}"
 
 # create $WRITEDIR if not assignment1
-assignment=`cat conf/assignment.txt`
+assignment=`cat ${CONFIG_PATH}/assignment.txt`
 
 if [ $assignment != 'assignment1' ]
 then
