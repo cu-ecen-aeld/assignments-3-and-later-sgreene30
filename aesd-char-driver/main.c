cacb572c227ccad06c09bcc3ec73a527fefd75d8
sgreene30 <sgreene30@Ubuntu-Fresh.myguest.virtualbox.org>
2023-10-01 15:17:38 -0400

    fixing copy to user bug in read

--- aesd-char-driver/main.c
+++ aesd-char-driver/main.c
@@ -80,7 +80,7 @@ ssize_t aesd_read(struct file *filp, char __user *buf, size_t count,loff_t *f_po
     retval = read_entry->size - find_entry_rtn;
     PDEBUG("circular buffer working entry was %s", read_entry->buffptr);
     //PDEBUG("copy_to_user");
-    if(copy_to_user(buf, (char *)read_entry->buffptr, retval) == 0)
+    if(copy_to_user(buf, (char *)read_entry->buffptr + find_entry_rtn, retval) == 0)
     {
         *f_pos += retval;
         PDEBUG("User space value was %s", buf);
